b1c698fe035f267b2a712d4d761373e9758e0e33
cakyus <cakyus@gmail.com>
2011-07-05 21:30:08 +0700

    use global  (already included in index.php, if exists)

--- library/Nomads/Config.php
+++ library/Nomads/Config.php
@@ -3,28 +3,19 @@
 class Nomads_Config {
 
     public function __construct () {
-    
-        $file = new Nomads_File;
+    	
+    	global $config;
+    	
+        if (isset($config[get_class($this)])) {
+        	$configClass = $config[get_class($this)];
+        } else {
+        	return false;
+        }
         
-        $fileConfig = FRAMEWORK_PATH.'/config.php';
-        
-        if ($file->open($fileConfig)) {
-            // fileConfig is found
-            // @todo this include is called multiple times
-            //  perhaps Singleton will be better
-            include($fileConfig);
-            
-            if (isset($config[get_class($this)])) {
-            	$configClass = $config[get_class($this)];
-            } else {
-            	return false;
-            }
-            
-            foreach ($this as $key => $value) {
-            	if (isset($configClass[$key])) {
-            		$this->$key = $configClass[$key];
-            	}
-            }
+        foreach ($this as $key => $value) {
+        	if (isset($configClass[$key])) {
+        		$this->$key = $configClass[$key];
+        	}
         }
     }
 }
