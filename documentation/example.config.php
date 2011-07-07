b00d805aa59f15329624fb5b69ae414dc0c279ab
cakyus <cakyus@gmail.com>
2011-07-07 18:49:48 +0700

    adjust files and folders into a library

--- documentation/example.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['Nomads_Database_Config']['hostname'] = '127.0.0.1';
-$config['Nomads_Database_Config']['username'] = 'root';
-$config['Nomads_Database_Config']['password'] = '';
-$config['Nomads_Database_Config']['database'] = 'nomads';
-
