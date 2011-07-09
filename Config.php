ac75efb9578d9cc52826bb7616745cad64b7b836
cakyus <cakyus@gmail.com>
2011-07-10 01:54:40 +0700

    config's keyword is no longer need _Config suffix

--- Config.php
+++ Config.php
@@ -6,8 +6,18 @@ class Nomads_Config {
     	
     	global $config;
     	
-        if (isset($config[get_class($this)])) {
-        	$configClass = $config[get_class($this)];
+    	$className = get_class($this);
+    	
+    	if (substr($className, -7) != '_Config') {
+    		trigger_error('Class name not ended with "_Config"');
+    		return false;
+    	}
+    	
+    	// remove __Config suffix from className
+    	$className = substr($className, 0, -7);
+    	
+        if (isset($config[$className])) {
+        	$configClass = $config[$className];
         } else {
         	return false;
         }
