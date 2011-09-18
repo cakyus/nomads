8fc041e4074fccacf20db12c2aa9431a071cf893
cakyus <cakyus@gmail.com>
2011-09-18 20:35:53 +0700

    new templating method

--- View.php
+++ View.php
@@ -6,67 +6,25 @@
  
 class Nomads_View {
 
-	private $title;
-	private $content;
-	private $styles;
-	private $scripts;
+	private $properties;
 	
 	public function __construct() {
-	    $this->styles = array();
-	    $this->scripts = array();
-	}
-
-	public function setTitle($title) {
-		$this->title = $title;
+	    $this->properties = array();
 	}
 	
-	public function addStyle($filePath) {
+	public function assign($keyword, $value) {
+		$this->properties[$keyword] = $value;
+	}
 	
-		$storage = new Nomads_Storage;
-		
-		$className = get_class($this);
-		$libraryName = substr($className, 0, strpos($className, '_'));
-		$filePath = $libraryName.'/'.$filePath;
-		
-		if ($storage->open($filePath)) {
-		    $this->styles[] = $storage->getURL();
+	public function render($file) {
+		include(APPLICATION_PATH.'/view/'.$file);
+	}
+	
+	public function __get($keyword) {
+		if (isset($this->properties[$keyword])) {
+			return $this->properties[$keyword];
 		}
-	}
-
-	public function addScript($URL) {
-	    $this->scripts[] = $URL;
-	}
-
-	public function add($string) {
-		$this->content .= $string;
-	}
-
-	public function write() {
-	
-		$config = new Nomads_Config;
-		
-		echo '<html><head>';
-		echo '<title>'.$this->title.'</title>';
-				
-		// styles
-		foreach ($this->styles as $style) {
-		    echo '<link rel="stylesheet" type="text/css"'
-		    	.' href="'.$style.'"'
-		    	.' />';    
-        }
-		// scripts
-		foreach ($this->scripts as $script) {
-		    echo '<script src="'.$storageURL.'/'.$script.'"></script>';    
-        }
-        
-		echo '</head><body><center><div id="theme">';
-		
-		if (empty($this->pageTitle) == false) {
-    		echo '<h1>'.$this->pageTitle.'</h1>';
-		}
-		
-		echo $this->content;
-		echo '</div></center></body></html>';
+		return '';
 	}
 }
 
