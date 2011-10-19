72506ccd6919f5a173aaba8da5a91ca95fd6b619
cakyus <cakyus@gmail.com>
2011-10-19 15:47:11 +0700

    remove view property from Controller

--- Controller.php
+++ Controller.php
@@ -1,15 +1,7 @@
 <?php
 
-/**
- * Nomads_Controller
- **/
- 
 class Nomads_Controller {
 	
-	public $view;
-
-	public function __construct () {
-		$this->view = new Nomads_View;
-	}
+	public function __construct () {}
 }
 
