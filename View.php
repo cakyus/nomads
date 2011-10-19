b3e662133a3cd5c1b51cde84de0cf6931a2a11d9
cakyus <cakyus@gmail.com>
2011-10-19 15:52:42 +0700

    Allow to set null value to $properties of Nomads_View

--- View.php
+++ View.php
@@ -3,17 +3,24 @@
 /**
  * Nomads_View
  **/
- 
+
 class Nomads_View {
 
 	private $properties;
 	
 	public function __construct() {
 	    $this->properties = array();
+		// special properties
+		$this->set('rootURL', dirname($_SERVER['SCRIPT_NAME']));
 	}
 	
-	public function set($keyword, $value) {
-		$this->properties[$keyword] = $value;
+	public function set($keyword, $value=null) {
+		
+		if (is_string($keyword)) {
+			$this->properties[$keyword] = $value;
+		} else {
+			$this->properties = array_merge($this->properties, (array)$keyword);
+		}
 	}
 	
 	public function show($file) {
