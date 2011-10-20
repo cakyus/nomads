841fbf85fbca0a745f972f7cfdcb9ada8d8ddf8b
cakyus <cakyus@gmail.com>
2011-10-20 12:49:51 +0700

    add Nomads_Loader::register()

--- Loader.php
+++ Loader.php
@@ -7,10 +7,23 @@ class Nomads_Loader {
 		return spl_autoload_register(array(__CLASS__, 'loadClass'));	
 	}
 	
+	public static function register() {
+		// add current folder in included path
+		self::addIncludePath(dirname(__DIR__));
+		return spl_autoload_register(array(__CLASS__, 'loadClass'));	
+	}
+
+	public static function addIncludePath($path) {
+		set_include_path(implode(PATH_SEPARATOR
+			, array($path, get_include_path())
+			));
+	}
+	
 	public static function loadClass($class) {
 		
 		// find in included path
 		$dirs = explode(PATH_SEPARATOR, get_include_path());
+		// echo get_include_path();
 		$path = str_replace('_','/',$class).'.php';
 		
 		foreach ($dirs as $dir) {
@@ -32,5 +45,23 @@ class Nomads_Loader {
 				return true;
 			}
 		}
+		
+		// model
+		$file = APPLICATION_PATH.'/model/'
+			.str_replace('_','/',$class)
+			.'.php'
+			;
+		if (file_exists($file)) {
+			require_once($file);
+			return true;
+		}
+		
+		// class already exists
+		if (in_array($class, get_declared_classes())) {
+			return true;
+		}
+		
+		// fail
+		trigger_error('Can\'t resolve class "'.$class.'"');
 	}
 }
