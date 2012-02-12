0eae2254c246b4b79a99c6c181f65ede431a2afb
cakyus <cakyus@gmail.com>
2012-02-12 11:45:05 +0700

    add properties overloading

--- Component.php
+++ Component.php
@@ -4,12 +4,31 @@ class Nomads_Component {
 
 	public function __construct() {}
 	
+	public function setProperties($object) {
+		foreach ($object as $name => $value) {
+			$this->$name = $value;
+		}	
+	}
+	
 	public function __get($name) {
-		
+		$getter = "get".$name;
+		if (method_exists($this, $getter)) {
+			return $this->$getter();
+		}
+		throw new Nomads_Exception("Property '$name' is not defined");
 	}
 
 	public function __set($name, $value) {
-		
+		$setter = "set" . $name;
+		if (method_exists($this, $setter)) {
+			return $this->$setter($value);
+		}
+
+		if (method_exists($this, "get".$name)) {
+			throw new CException("Property '$name' is read only.");
+		} else {
+			throw new CException("Property '$name' is not defined.");
+		}
 	}
 }
 
